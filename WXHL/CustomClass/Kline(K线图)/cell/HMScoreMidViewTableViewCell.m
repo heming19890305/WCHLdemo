8f00e48780c42d3e8931a72a9537cc748ffa4fed
Tomorrow <1335969773@qq.com>
2018-03-21 16:47:22 +0800

    1.重构卖出工分页面；2头部控件布局；3.中间控件布局

--- WXHL/CustomClass/Kline(K线图)/cell/HMScoreMidViewTableViewCell.m
+++ WXHL/CustomClass/Kline(K线图)/cell/HMScoreMidViewTableViewCell.m
@@ -10,16 +10,199 @@
 
 @implementation HMScoreMidViewTableViewCell
 
-- (void)awakeFromNib {
-    [super awakeFromNib];
-    // Initialization code
-    NSLog(@"bibi");
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    if (self) {
+        self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+        //添加中间控件
+        [self setupMidView];
+    }
+    return self;
 }
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
+//添加中间控件
+- (void)setupMidView
+{
+    
+    //1.背景View
+    UIView * BgView = [UIView new];
+    BgView.backgroundColor = LINECOLOR;
+    [self addSubview:BgView];
+    BgView.sd_layout
+    .topSpaceToView(self, 0)
+    .bottomEqualToView(self)
+    .leftEqualToView(self)
+    .rightEqualToView(self);
+    //2.添加订单数据
+    //2.1添加“订单”label
+    UILabel * orderLabel = [UILabel new];
+    orderLabel.text = @"订单";
+    orderLabel.textColor = [UIColor blueColor];
+    orderLabel.font = [UIFont systemFontOfSize:18.0];
+    [BgView addSubview:orderLabel];
+    orderLabel.sd_layout
+    .topSpaceToView(BgView, 20)
+    .leftSpaceToView(BgView, 10)
+    .widthIs(60)
+    .heightIs(25);
+    //2.2订单数据view
+    UIView * orderView = [UIView new];
+    orderView.backgroundColor = [UIColor whiteColor];
+    [BgView addSubview:orderView];
+    orderView.sd_layout
+    .topSpaceToView(orderLabel, 6)
+    .leftEqualToView(BgView)
+    .rightEqualToView(BgView)
+    .heightIs(50);
+    //2.3 订单笔数Label
+    UILabel * orderNumLabel = [UILabel new];
+    orderNumLabel.text = @"今日订单（笔）：";
+    orderNumLabel.font = [UIFont systemFontOfSize:16.0];
+    [orderView addSubview:orderNumLabel];
+    CGSize orderNumLabelSize = [self addLabel:orderNumLabel string:orderNumLabel.text];
+    orderNumLabel.sd_layout
+    .topEqualToView(orderView)
+    .bottomEqualToView(orderView)
+    .leftSpaceToView(orderView, 10)
+    .widthIs(orderNumLabelSize.width);
+    //2.4订单数据Label
+    UILabel * orderDataLabel = [UILabel new];
+    float orderData = 2253;
+    orderDataLabel.text = [self data:orderData];
+    orderDataLabel.font = [UIFont systemFontOfSize:16.0];
+    [orderView addSubview:orderDataLabel];
+    CGSize orderDataLabelSize = [self addLabel:orderNumLabel string:orderDataLabel.text];
+    orderDataLabel.sd_layout
+    .topEqualToView(orderView)
+    .bottomEqualToView(orderView)
+    .leftSpaceToView(orderNumLabel, 2)
+    .widthIs(orderDataLabelSize.width);
+    //2.5订单金额数据Label
+    UILabel * orderAmountDataLabel = [UILabel new];
+    float orderAmountData = 225233;
+    orderAmountDataLabel.text = [self data:orderAmountData];
+    orderAmountDataLabel.font = [UIFont systemFontOfSize:16.0];
+    [orderView addSubview:orderAmountDataLabel];
+    CGSize orderAmountDataLabelSize = [self addLabel:orderAmountDataLabel string:orderAmountDataLabel.text];
+    orderAmountDataLabel.sd_layout
+    .topEqualToView(orderView)
+    .bottomEqualToView(orderView)
+    .rightSpaceToView(orderView, 10)
+    .widthIs(orderAmountDataLabelSize.width);
+    //2.6 订单金额Label
+    UILabel * orderAmountabel = [UILabel new];
+    orderAmountabel.text = @"订单合计（元）：";
+    orderAmountabel.font = [UIFont systemFontOfSize:16.0];
+    [orderView addSubview:orderAmountabel];
+    CGSize orderAmountabelSize = [self addLabel:orderAmountabel string:orderAmountabel.text];
+    orderAmountabel.sd_layout
+    .topEqualToView(orderView)
+    .bottomEqualToView(orderView)
+    .rightSpaceToView(orderAmountDataLabel, 2)
+    .widthIs(orderAmountabelSize.width);
+    
+    
+    
+    //3.添加分成数据
+    //3.1添加“分成”label
+    UILabel * boundsLabel = [UILabel new];
+    boundsLabel.text = @"分成";
+    boundsLabel.textColor = [UIColor blueColor];
+    boundsLabel.font = [UIFont systemFontOfSize:18.0];
+    [BgView addSubview:boundsLabel];
+    boundsLabel.sd_layout
+    .topSpaceToView(orderView, 20)
+    .leftSpaceToView(BgView, 10)
+    .widthIs(60)
+    .heightIs(25);
+    //3.2分成数据view
+    UIView * boundsView = [UIView new];
+    boundsView.backgroundColor = [UIColor whiteColor];
+    [BgView addSubview:boundsView];
+    boundsView.sd_layout
+    .topSpaceToView(boundsLabel, 6)
+    .leftEqualToView(BgView)
+    .rightEqualToView(BgView)
+    .heightIs(50);
+    //3.3 订单笔数Label
+    UILabel * orderBoundsLabel = [UILabel new];
+    orderBoundsLabel.text = @"分成工分（个）：";
+    orderBoundsLabel.font = [UIFont systemFontOfSize:16.0];
+    [boundsView addSubview:orderBoundsLabel];
+    CGSize orderBoundsLabelSize = [self addLabel:orderBoundsLabel string:orderBoundsLabel.text];
+    orderBoundsLabel.sd_layout
+    .topEqualToView(boundsView)
+    .bottomEqualToView(boundsView)
+    .leftSpaceToView(boundsView, 10)
+    .widthIs(orderBoundsLabelSize.width);
+    //3.4分成工分个数数据Label
+    UILabel * orderBoundsDataLabel = [UILabel new];
+    float orderBoundsData = 2253;
+    orderBoundsDataLabel.text = [self data:orderBoundsData];
+    orderBoundsDataLabel.font = [UIFont systemFontOfSize:16.0];
+    [boundsView addSubview:orderBoundsDataLabel];
+    CGSize orderBoundsDataLabelSize = [self addLabel:orderBoundsDataLabel string:orderBoundsDataLabel.text];
+    orderBoundsDataLabel.sd_layout
+    .topEqualToView(boundsView)
+    .bottomEqualToView(boundsView)
+    .leftSpaceToView(orderBoundsLabel, 2)
+    .widthIs(orderBoundsDataLabelSize.width);
+    //3.5分成金额数据Label
+    UILabel * boundsDataLabel = [UILabel new];
+    float boundsAmountData = 2233;
+//    _amount = boundsAmountData;
+    boundsDataLabel.text = [self data:boundsAmountData];
+    boundsDataLabel.font = [UIFont systemFontOfSize:16.0];
+    [boundsView addSubview:boundsDataLabel];
+    CGSize boundsDataLabelSize = [self addLabel:boundsDataLabel string:boundsDataLabel.text];
+    boundsDataLabel.sd_layout
+    .topEqualToView(boundsView)
+    .bottomEqualToView(boundsView)
+    .rightSpaceToView(boundsView, 10)
+    .widthIs(boundsDataLabelSize.width);
+    //3.6 分成金额Label
+    UILabel * boundsAmountabel = [UILabel new];
+    boundsAmountabel.text = @"分成金额（元）：";
+    boundsAmountabel.font = [UIFont systemFontOfSize:16.0];
+    [boundsView addSubview:boundsAmountabel];
+    CGSize boundsAmountabelSize = [self addLabel:boundsAmountabel string:boundsAmountabel.text];
+    boundsAmountabel.sd_layout
+    .topEqualToView(boundsView)
+    .bottomEqualToView(boundsView)
+    .rightSpaceToView(boundsDataLabel, 2)
+    .widthIs(boundsAmountabelSize.width);
+//    //计算分成Btn
+//    UIButton *boundsBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+//    boundsBtn.backgroundColor = [UIColor clearColor];
+//    [boundsBtn addTarget:self action:@selector(boundsClick:) forControlEvents:UIControlEventTouchUpInside];
+//    [boundsBtn setTitleColor:[UIColor blueColor] forState:0];
+//    [boundsBtn setTitle:@"计算分成" forState:0];
+//    boundsBtn.layer.cornerRadius = 8;
+//    boundsBtn.layer.masksToBounds = YES;
+//    boundsBtn.layer.borderColor = [[UIColor blueColor] CGColor];
+//    boundsBtn.layer.borderWidth = 1;
+//    [BgView addSubview:boundsBtn];
+    //        sellBtn.sd_layout.topEqualToView(_num, HEIGHT_TO(31)).lefts
+//    boundsBtn.sd_layout
+//    .topSpaceToView(boundsView, HEIGHT_TO(31))
+//    .centerXEqualToView(BgView)
+//    .widthIs(WIDTH_TO(220))
+//    .heightIs(HEIGHT_TO(38));
+    
 }
-
+//自适应UILabel 大小
+- (CGSize)addLabel:(UILabel *)label string:(NSString *)string {
+    CGSize widthSize = [string boundingRectWithSize:CGSizeMake(MAXFLOAT, label.frame.size.height) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName:label.font} context:nil].size;
+    return widthSize;
+}
+//数据长度
+- (NSString *)data:(float) data{
+    NSString * dataStr;
+    if (data > 10000) {
+        data = data / 10000;
+        dataStr = [NSString stringWithFormat:@"%0.2f 万",data];
+    }else{
+        dataStr = [NSString stringWithFormat:@"%0.f ",data];
+    }
+    return dataStr;
+}
 @end
--- WXHL/CustomClass/Kline(K线图)/controller/HMScoreViewController.m
+++ WXHL/CustomClass/Kline(K线图)/controller/HMScoreViewController.m
@@ -15,23 +15,28 @@
 @interface HMScoreViewController ()<UITableViewDelegate, UITableViewDataSource>
 @property (nonatomic, strong) UITableView * tableView;
 //顶部工分数据
-@property (nonatomic, strong) NSMutableArray * topScoreDataArr;
+
 //订单数据
-@property (nonatomic, strong) NSMutableArray * midOrderDataArr;
+
 //分成数据
-@property (nonatomic, strong) NSMutableArray * BottomBoundsDataArr;
+
+
+@property (nonatomic, strong) UIView * headerView;
 @end
 
 @implementation HMScoreViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    self.title = @"工分";
     self.backBtn.hidden = YES;
+    self.view.backgroundColor = [UIColor yellowColor];
     //1.登陆状态判断
     //2.初始化子控件
     [self setupView];
-    //3.获取数据
+    //3.刷新数据
+    [self setRefresh];
+    //4.获取数据
     [self getData];
     
 }
@@ -39,16 +44,17 @@
 - (void)setupView
 {
     self.view.backgroundColor = [UIColor greenColor];
-    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT - 64 -TAB_HEIGHT) style:UITableViewStylePlain];
-    self.tableView.tableHeaderView.frame = CGRectMake(0, 0, 400, 200);
-   
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT - 64 -TAB_HEIGHT - 150) style:UITableViewStylePlain];
+//    self.tableView.tableHeaderView.frame = CGRectMake(0, 0, 400, 200);
+    self.tableView.backgroundColor = LINECOLOR;
     
     self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
     
-    //注册 TabbleViewcell
-    [self.tableView registerClass:[HMScoreTopViewTableViewCell class] forCellReuseIdentifier:TOP_CELL];
+    self.tableView.tableHeaderView = self.headerView;
+//    注册 TabbleViewcell
+
     [self.tableView registerClass:[HMScoreMidViewTableViewCell class] forCellReuseIdentifier:MID_CELL];
     
     if ([self.tableView respondsToSelector:@selector(setContentInsetAdjustmentBehavior:)]) {
@@ -62,20 +68,202 @@
             self.automaticallyAdjustsScrollViewInsets = NO;
         }
     }
+//    [self.view addSubview:self.tableView];
     [self.view addSubview:self.tableView];
-    
    
     self.tableView.mj_header = [MJRefreshNormalHeader headerWithRefreshingBlock:^{
-        
+
         [self getData];
     }];
+}
+//刷新数据
+- (void)setRefresh
+{
+    [self.tableView.mj_header endRefreshing];
+    [self.tableView.mj_footer endRefreshing];
 }
 //获取数据
 
 - (void)getData
 {
-    
+    NSLog(@"获取数据");
 }
 
+//headerView
+- (UIView *)headerView
+{
+    if (!_headerView) {
+        _headerView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, SCREENWIDTH, HEIGHT_TO(200))];
+        UIImageView *img = [[UIImageView alloc]initWithImage:ImageNamed(@"xf_top_bj")];
+        [_headerView addSubview:img];
+        img.sd_layout
+        .leftSpaceToView(_headerView, 0)
+        .rightSpaceToView(_headerView, 0)
+        .topSpaceToView(_headerView, 0)
+        .heightIs(HEIGHT_TO(200));
+        //2.添加已获得工分个数
+        UILabel *gotScoreLabel = [UILabel new];
+        gotScoreLabel.textColor = [UIColor whiteColor];
+        NSString * labelText = @"当前工分";
+        gotScoreLabel.text = labelText;
+        gotScoreLabel.font = [UIFont boldSystemFontOfSize:14.0f];
+        //自适应UILabel 大小
+        CGSize labelSize = [self addLabel:gotScoreLabel string:labelText];
+        [_headerView addSubview:gotScoreLabel];
+        gotScoreLabel.sd_layout
+        .topSpaceToView(_headerView, 15)
+        .centerXEqualToView(_headerView)
+        .widthIs(labelSize.width)
+        .heightIs(labelSize.height);
+        //2.1工分个数
+        UILabel * gotScore = [UILabel new];
+        gotScore.textColor = [UIColor whiteColor];
+        double c = 3628428;
+        NSString * str ;
+        if (c > 10000) {
+            c = c / 10000;
+            str = [NSString stringWithFormat:@"%0.2f 万",c];
+        }else
+        {
+            str = [NSString stringWithFormat:@"%0.f",c];
+        }
+        gotScore.font = [UIFont systemFontOfSize:28.0];
+        //自适应UILabel 大小
+        CGSize gotScoreSize = [self addLabel:gotScore string:str];
+        
+        gotScore.numberOfLines = 0;
+        [_headerView addSubview:gotScore];
+        gotScore.sd_layout
+        .topSpaceToView(gotScoreLabel, 3)
+        .centerXEqualToView(_headerView)
+        .widthIs(gotScoreSize.width)
+        .heightIs(gotScoreSize.height);
+        
+        //3.添加未获得工分个数
+        //3.0 未获得工分label
+        UILabel * unGotSoreLabel = [UILabel new];
+        unGotSoreLabel.textColor = [UIColor whiteColor];
+        NSString * unGotStr = @"未获得工分";
+        unGotSoreLabel.text = unGotStr;
+        unGotSoreLabel.font =[UIFont boldSystemFontOfSize:14.f];
+        CGSize unGotLabelSize = [self addLabel:unGotSoreLabel string:unGotStr];
+        [_headerView addSubview:unGotSoreLabel];
+        unGotSoreLabel.sd_layout
+        .topSpaceToView(gotScore, 15)
+        .centerXEqualToView(_headerView)
+        .widthIs(unGotLabelSize.width)
+        .heightIs(unGotLabelSize.height);
+        //3.1未获得工分num
+        UILabel * unGotScore = [UILabel new];
+        unGotScore.textColor = [UIColor whiteColor];
+        double d = 12345678;
+        NSString * unStr;
+        if (d > 10000) {
+            d = d / 10000;
+            unStr = [NSString stringWithFormat:@"%0.2f 万",d];
+        }else{
+            unStr = [NSString stringWithFormat:@"%0.f",d];
+        }
+        unGotScore.font = [UIFont systemFontOfSize:16.0];
+        CGSize unGotScoreSize = [self addLabel:unGotScore string:unStr];
+        unGotScore.text = unStr;
+        unGotScore.numberOfLines = 0;
+        [_headerView addSubview:unGotScore];
+        unGotScore.sd_layout
+        .topSpaceToView(unGotSoreLabel, 3)
+        .centerXEqualToView(_headerView)
+        .widthIs(unGotScoreSize.width)
+        .heightIs(unGotScoreSize.height);
+        //4.卖出按钮
+        UIButton *sellBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        sellBtn.backgroundColor = [UIColor clearColor];
+        [sellBtn addTarget:self action:@selector(sellClick) forControlEvents:UIControlEventTouchUpInside];
+        [sellBtn setTitleColor:[UIColor whiteColor] forState:0];
+        [sellBtn setTitle:@"卖出工分" forState:0];
+        sellBtn.layer.cornerRadius = 5;
+        sellBtn.layer.masksToBounds = YES;
+        sellBtn.layer.borderColor = [[UIColor whiteColor] CGColor];
+        sellBtn.layer.borderWidth = 1;
+        [_headerView addSubview:sellBtn];
+        //        sellBtn.sd_layout.topEqualToView(_num, HEIGHT_TO(31)).lefts
+        sellBtn.sd_layout
+        .topSpaceToView(unGotScore, HEIGHT_TO(31))
+        .centerXEqualToView(_headerView)
+        .widthIs(WIDTH_TO(120))
+        .heightIs(HEIGHT_TO(38));
+        
+        //5.明细
+        UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [btn setTitle:@"明细" forState:0];
+        //    [btn addTarget:self action:@selector(showDetail) forControlEvents:UIControlEventTouchUpInside];
+        [btn setTitleColor:[UIColor whiteColor] forState:0];
+        btn.backgroundColor = [UIColor clearColor];
+        [_headerView addSubview:btn];
+        btn.sd_layout
+        .topSpaceToView(_headerView, 10)
+        .rightSpaceToView(_headerView,0)
+        .heightIs(25)
+        .widthIs(WIDTH_TO(60));
+
+    }
+    return _headerView;
+}
+- (void)sellClick
+{
+    NSLog(@"卖出工分");
+}
+//自适应UILabel 大小
+- (CGSize)addLabel:(UILabel *)label string:(NSString *)string {
+    CGSize widthSize = [string boundingRectWithSize:CGSizeMake(MAXFLOAT, label.frame.size.height) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName:label.font} context:nil].size;
+    return widthSize;
+}
+//数据长度
+- (NSString *)data:(float) data{
+    NSString * dataStr;
+    if (data > 10000) {
+        data = data / 10000;
+        dataStr = [NSString stringWithFormat:@"%0.2f 万",data];
+    }else{
+        dataStr = [NSString stringWithFormat:@"%0.f ",data];
+    }
+    return dataStr;
+}
+#pragma mark - delegate
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+//    self.tableView.footer.hidden = (self.topics.count == 0);
+//    return self.topics.count;
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    
+//    HMWordCell *cell = [tableView dequeueReusableCellWithIdentifier:HMWordCellID];
+//
+//    cell.word = self.topics[indexPath.row];
+    //    cell.textLabel.text = word.name;
+    //    cell.detailTextLabel.text = word.text;
+    //    [cell.imageView sd_setImageWithURL:[NSURL URLWithString:word.profile_image] placeholderImage:[UIImage imageNamed:@"defaultUserIcon"]];
+    //    HMLog(@"cellForRowAtIndexPat--------");
+//    return cell;
+    HMScoreMidViewTableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:MID_CELL];
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    //取出帖子模型
+//    HMWord * word = self.topics[indexPath.row];
+    //      HMLog(@"heightForRowAtIndexPath++++++++");
+//    return word.cellHeight;
+    return 50;
+}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+//    HMCommentViewController * commentVC = [[HMCommentViewController alloc] init];
+//    commentVC.topic = self.topics[indexPath.row];
+//    [self.navigationController pushViewController:commentVC animated:YES];
+}
 
 @end
